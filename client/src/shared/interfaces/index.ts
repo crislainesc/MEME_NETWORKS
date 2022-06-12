d34104570e2327bd6d12225b2a98632e92a8c4db
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-06-12 12:36:30 -0300

    feat(client): interfaces and types added

---
+++ client/src/shared/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface IMemeCreated {
+  id: string;
+  name: string;
+  url: string;
+  character: string;
+  topText: string;
+  topTextSize: number;
+  buttonText: string;
+  buttonTextSize: number;
+  capitalLetters: boolean;
+}
