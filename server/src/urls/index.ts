ffedb68e95395d00e7b9157d44955a4f694cf007
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-06-15 22:29:30 -0300

    feat(server): base urls added

---
+++ server/src/urls/index.ts
@@ -0,0 +1,8 @@
+export const showUrl =
+  "https://meme-generator-rc-default-rtdb.firebaseio.com/memes/generated";
+
+export const searchUrl =
+  "https://meme-generator-rc-default-rtdb.firebaseio.com/memes/images";
+
+export const createUrl =
+  "https://meme-generator-rc-default-rtdb.firebaseio.com/memes/generated";
